9a7130f4f646ddc4b936653458c418f11b2f1cca
davidrt <jdramirezt@unal.edu.co>
2024-12-18 18:30:22 -0500

    App.,js con todo

--- src/App.js
+++ src/App.js
@@ -1,43 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
-import ListPersonasComponent from './components/Persona/ListPersonasComponent';
-import ListViviendaComponent from './components/Vivienda/ListViviendaComponent';
-import ListMunicipioComponent from './components/Municipio/ListMunicipioComponent';
-import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AddPersonasComponent from './components/Persona/AddPersonasComponent';
-import AddViviendaComponent from './components/Vivienda/AddViviendaComponent';
-import AddMunicipioComponent from './components/Municipio/AddMunicipioComponent';
-import EditViviendaComponent from './components/Vivienda/EditViviendaComponent';
-import EditPersonasComponent from './components/Persona/EditPersonasComponent';
-import EditMunicipioComponent from './components/Municipio/EditMunicipioComponent';
+import HeaderComponent from './components/HeaderComponent';
 import HomeComponent from './components/HomeComponent';
 
-function App() {
+import ListPersonasComponent from './components/Persona/ListPersonasComponent';
+import AddPersonaComponent from './components/Persona/AddPersonasComponent';
 
+import ListViviendaComponent from './components/Vivienda/ListViviendaComponent';
+import AddViviendaComponent from './components/Vivienda/AddViviendaComponent';
+
+import ListMunicipioComponent from './components/Municipio/ListMunicipioComponent';
+import AddMunicipioComponent from './components/Municipio/AddMunicipioComponent';
+
+import ListCDFComponent from './components/CDF/ListCDFComponent';
+import AddCDFComponent from './components/CDF/AddCDFComponent';
+
+import ListGobernadorComponent from './components/Gobernador/ListGobernadorComponent';
+import AddGobernadorComponent from './components/Gobernador/AddGobernadorComponent';
+
+import ListPosesionComponent from './components/Posesion/ListPosesionComponent';
+import AddPosesionComponent from './components/Posesion/AddPosesionComponent';
+
+function App() {
   return (
     <div>
       <BrowserRouter>
         <HeaderComponent />
         <div className='container'>
           <Routes>
-            <Route exact path='/' element={<HomeComponent />}></Route>
-            <Route path='/home' element={<HomeComponent />}></Route>
-            <Route path='/persona' element={<ListPersonasComponent />}></Route>
-            <Route path='/add-persona' element={<AddPersonasComponent />}></Route>
-            <Route path='/edit-persona/:id' element={<EditPersonasComponent />}></Route>
-            <Route path='/vivienda' element={<ListViviendaComponent />}></Route>
-            <Route path='/add-vivienda' element={<AddViviendaComponent />}></Route>
-            <Route path='/edit-vivienda/:id' element={<EditViviendaComponent />}></Route>
-            <Route path='/municipio' element={<ListMunicipioComponent />}></Route>
-            <Route path='/add-municipio' element={<AddMunicipioComponent />}></Route>
-            <Route path='/edit-municipio/:id' element={<EditMunicipioComponent />}></Route>
+            {/* Home */}
+            <Route exact path='/' element={<HomeComponent />} />
+            <Route path='/home' element={<HomeComponent />} />
+
+            {/* Persona */}
+            <Route path='/persona' element={<ListPersonasComponent />} />
+            <Route path='/add-persona' element={<AddPersonaComponent />} />
+
+            {/* Vivienda */}
+            <Route path='/vivienda' element={<ListViviendaComponent />} />
+            <Route path='/add-vivienda' element={<AddViviendaComponent />} />
+
+            {/* Municipio */}
+            <Route path='/municipio' element={<ListMunicipioComponent />} />
+            <Route path='/add-municipio' element={<AddMunicipioComponent />} />
+
+            {/* CDF */}
+            <Route path='/cdf' element={<ListCDFComponent />} />
+            <Route path='/add-cdf' element={<AddCDFComponent />} />
+
+            {/* Gobernador */}
+            <Route path='/gobernador' element={<ListGobernadorComponent />} />
+            <Route path='/add-gobernador' element={<AddGobernadorComponent />} />
+
+            {/* Posesión */}
+            <Route path='/posesion' element={<ListPosesionComponent />} />
+            <Route path='/add-posesion' element={<AddPosesionComponent />} />
           </Routes>
         </div>
       </BrowserRouter>
     </div>
-
   );
 }
+
 export default App;
