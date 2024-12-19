ee6e0aa704d491d6277204d584ee10bab9fa25ce
davidrt <jdramirezt@unal.edu.co>
2024-12-19 10:53:53 -0500

    routes correction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import ListPersonasComponent from './components/Persona/ListPersonasComponent';
 import ListViviendaComponent from './components/Vivienda/ListViviendaComponent';
 import ListMunicipioComponent from './components/Municipio/ListMunicipioComponent';
 import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPersonasComponent from './components/Persona/AddPersonasComponent';
 import AddViviendaComponent from './components/Vivienda/AddViviendaComponent';
@@ -13,7 +11,20 @@ import EditViviendaComponent from './components/Vivienda/EditViviendaComponent';
 import EditPersonasComponent from './components/Persona/EditPersonasComponent';
 import EditMunicipioComponent from './components/Municipio/EditMunicipioComponent';
 import HomeComponent from './components/HomeComponent';
+import ListCDFComponent from './components/CDF/ListCDFComponent';
+import AddCDFComponent from './components/CDF/AddCDFComponent';
+//import EditCDFComponent from './components/CDF/EditCDFComponent';
+import ListGobernadorComponent from './components/Gobernador/ListGobernadorComponent';
+import AddGobernadorComponent from './components/Gobernador/AddGobernadorComponent';
+//import EditGobernadorComponent from './components/Gobernador/EditGobernadorComponent';
+import ListPosesionComponent from './components/Posesion/ListPosesionComponent';
+import AddPosesionComponent from './components/Posesion/AddPosesionComponent';
+//import EditPosesionesComponent from './components/Posesiones/EditPosesionesComponent';
 
+
+//<Route path='/edit-cdf/:id' element={<EditCDFComponent />}></Route>
+//<Route path='/edit-posesiones/:id' element={<EditPosesionesComponent />}></Route>
+//<Route path='/edit-gobernador/:id' element={<EditGobernadorComponent />}></Route>
 function App() {
 
   return (
@@ -33,6 +44,13 @@ function App() {
             <Route path='/municipio' element={<ListMunicipioComponent />}></Route>
             <Route path='/add-municipio' element={<AddMunicipioComponent />}></Route>
             <Route path='/edit-municipio/:id' element={<EditMunicipioComponent />}></Route>
+            <Route path='/cdf' element={<ListCDFComponent />}></Route>
+            <Route path='/add-cdf' element={<AddCDFComponent />}></Route>
+            <Route path='/gobernadore' element={<ListGobernadorComponent />}></Route>
+            <Route path='/add-gobernador' element={<AddGobernadorComponent />}></Route>
+            <Route path='/posesiones' element={<ListPosesionComponent />}></Route>
+            <Route path='/add-posesion' element={<AddPosesionComponent />}></Route>
+
           </Routes>
         </div>
       </BrowserRouter>
